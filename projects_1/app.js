6a72003431b05f5182f0f9a0242deaf0fe06d064
RHsanto <rakibulsanto789@gmail.com>
2022-05-27 17:48:09 +0600

    add project 1d

--- projects_1/app.js
+++ projects_1/app.js
@@ -3,19 +3,28 @@ window.onload=()=>{
 };
 
 function main(){
- const root = document.getElementById('root')
- const button= document.getElementById('change-btn')
+ const items1 = document.getElementById('item1')
+ const items2 = document.getElementById('item2')
+ const items3 = document.getElementById('item3')
+ const items4 = document.getElementById('item4')
+ const button1 = document.getElementById('i-bnt1')
 
- button.addEventListener('click',function(){
-  root.style.backgroundColor=createColor()
+
+ button1.addEventListener('click',function(){
+  items1.style.backgroundColor=createColor()
+  items2.style.backgroundColor=createColor()
+  items3.style.backgroundColor=createColor()
+  items4.style.backgroundColor=createColor()
  })
 }
+
 
 
 function createColor(){
   const red = Math.floor(Math.random()*255)
   const blue = Math.floor(Math.random()*255)
   const green = Math.floor(Math.random()*255)
+
 
   return `rgb(${red},${blue},${green})`
 }
